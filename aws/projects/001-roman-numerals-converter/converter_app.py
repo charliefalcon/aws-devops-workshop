717c4464b5a4e884ff0b0b20b7793ba0a7a4a7d5
unknown <charliefalcon02@gmail.com>
2020-09-16 13:44:04 +0300

    converter app update

--- aws/projects/001-roman-numerals-converter/converter_app.py
+++ aws/projects/001-roman-numerals-converter/converter_app.py
@@ -29,5 +29,5 @@ def index():
         
 
 if __name__ == '__main__':
-    converter_app.run(debug = True)
-    #converter_app.run(host='0.0.0.0', port=80)
+    #converter_app.run(debug = True)
+    converter_app.run(host='0.0.0.0', port=80)
